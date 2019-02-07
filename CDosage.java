3829aab64f862a9a9a0b7912ae2984b3b1434307
Coco101010 <45937869+Coco101010@users.noreply.github.com>
2019-02-07 15:09:07 +0100

    Add files via upload

---
+++ CDosage.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entites;
+
+/**
+ *
+ * @author admin
+ */
+public class CDosage {
+    private int codeDosage;
+    private float quantite;
+    private String unite;
+
+    public int getCodeDosage() {
+        return codeDosage;
+    }
+
+    public void setCodeDosage(int codeDosage) {
+        this.codeDosage = codeDosage;
+    }
+
+    public float getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(float quantite) {
+        this.quantite = quantite;
+    }
+
+    public String getUnite() {
+        return unite;
+    }
+
+    public void setUnite(String unite) {
+        this.unite = unite;
+    }
+}
